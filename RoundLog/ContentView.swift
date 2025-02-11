c686e7639e0f4048f90c6c41be549486b4160fae
SatoshiUsui <usui.satoshi@gmail.com>
2025-02-11 18:31:20 +0900

    Initial Commit

---
+++ RoundLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoundLog
+//
+//  Created by 臼井智 on 2025/02/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RoundLog/RoundLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoundLogApp.swift
+//  RoundLog
+//
+//  Created by 臼井智 on 2025/02/11.
+//
+
+import SwiftUI
+
+@main
+struct RoundLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
