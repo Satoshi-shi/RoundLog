cbc5fdcb354622d99c6182e6b6256a0a6e11d933
SatoshiUsui <usui.satoshi@gmail.com>
2025-02-21 00:23:54 +0900

    2/20 構造化＆データ追加

---
+++ RoundLog/TestDatePickerDeletable.swift
@@ -0,0 +1,18 @@
+//
+//  TestDatePickerDeletable.swift
+//  RoundLog
+//
+//  Created by 臼井智 on 2025/02/18.
+//
+
+import SwiftUI
+
+struct TestDatePickerDeletable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestDatePickerDeletable()
+}
