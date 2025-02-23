3305869f97155b082fd1e40b2939b9cc64c6c6c6
SatoshiUsui <usui.satoshi@gmail.com>
2025-02-23 22:51:13 +0900

    2/23 RoundResultViewの新設まで

---
+++ RoundLog/RoundHoleView.swift
@@ -0,0 +1,18 @@
+//
+//  RoundHoleView.swift
+//  RoundLog
+//
+//  Created by 臼井智 on 2025/02/23.
+//
+
+import SwiftUI
+
+struct RoundHoleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoundHoleView()
+}
---
+++ RoundLog/RoundResultListView.swift
@@ -0,0 +1,18 @@
+//
+//  RoundResultListView.swift
+//  RoundLog
+//
+//  Created by 臼井智 on 2025/02/22.
+//
+
+import SwiftUI
+
+struct RoundResultListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoundResultListView()
+}
